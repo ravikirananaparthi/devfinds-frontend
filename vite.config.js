6c5c16a5321224c232746fa58b1a0f9928acc899
ravikirananaparthi <anaparthiravikiran20037@gmail.com>
2024-04-04 00:29:25 +0530

    Hello

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173, // Specify the port you want to use
+    // host: '0.0.0.0', // Uncomment this line if you want to listen on all network interfaces
+  },
 })
